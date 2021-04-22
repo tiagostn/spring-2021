df4b3e98143b2535286a0ee3a6fca751e535ecd6
tiagostn <tiagostn@senado.leg.br>
2021-04-22 00:25:52 -0300

    setup database and data-jpa

--- src/main/java/org/ts/spring21/controller/SpaceShipController.java
+++ src/main/java/org/ts/spring21/controller/SpaceShipController.java
@@ -1,9 +1,9 @@
 package org.ts.spring21.controller;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.ts.spring21.model.SpaceShip;
 import org.ts.spring21.service.SpaceShipService;
 
@@ -21,8 +21,33 @@ public class SpaceShipController {
     }
 
     @GetMapping
-    public List<SpaceShip> info() {
+    public List<SpaceShip> list() {
         log.info("List All");
         return spaceShipService.listAll();
     }
+
+    @GetMapping("/{id}")
+    public SpaceShip findById(@PathVariable("id") long id) {
+        return spaceShipService.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<SpaceShip> save(@RequestBody SpaceShip spaceShip) {
+        return new ResponseEntity<>(spaceShipService.save(spaceShip), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        spaceShipService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
+    @PutMapping
+    public ResponseEntity<SpaceShip> update(@RequestBody SpaceShip spaceShip) {
+        return ResponseEntity.ok(spaceShipService.save(spaceShip));
+    }
+
+
+
 }
--- src/main/java/org/ts/spring21/service/SpaceShipService.java
+++ src/main/java/org/ts/spring21/service/SpaceShipService.java
@@ -1,13 +1,48 @@
 package org.ts.spring21.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import org.ts.spring21.model.SpaceShip;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class SpaceShipService {
-    public List<SpaceShip> listAll(){
-        return List.of(new SpaceShip(1l,"Saturn V"), new SpaceShip(2l,"Falcon IV"));
+
+
+    private static final List<SpaceShip> spaceShips;
+
+    static {
+        spaceShips = new ArrayList<>(List.of(new SpaceShip(1L, "Saturn V"), new SpaceShip(2L, "Falcon IV")));
+    }
+
+    public List<SpaceShip> listAll() {
+        return spaceShips;
+    }
+
+    public SpaceShip findById(Long id) {
+        return spaceShips.stream()
+                .filter(s -> s.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Space not found"));
+    }
+
+    public SpaceShip save(SpaceShip spaceShip) {
+        if (spaceShip.getId() == null) {
+            spaceShip.setId(ThreadLocalRandom.current().nextLong(3, 9999));
+            spaceShips.add(spaceShip);
+        } else {
+            spaceShips.remove(findById(spaceShip.getId()));
+            spaceShips.add(spaceShip);
+        }
+        return spaceShip;
+    }
+
+    public void delete(long id) {
+        SpaceShip spaceShip = findById(id);
+        spaceShips.remove(spaceShip);
     }
 }
