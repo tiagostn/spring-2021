609b6d88c9b4fd533fe44c00f0bdf580b852c673
tiagostn <tiagostn@senado.leg.br>
2021-04-22 03:46:05 -0300

    adding validation exception details

--- src/main/java/org/ts/spring21/handler/RestExceptionHandler.java
+++ src/main/java/org/ts/spring21/handler/RestExceptionHandler.java
@@ -1,25 +1,53 @@
 package org.ts.spring21.handler;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.WebUtils;
 import org.ts.spring21.controller.response.ApiResponse;
 import org.ts.spring21.exception.BusinessException;
 import org.ts.spring21.exception.BusinessExceptionDetails;
+import org.ts.spring21.exception.ExceptionDetails;
 import org.ts.spring21.exception.ValidationExceptionDetails;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.springframework.web.context.request.RequestAttributes.SCOPE_REQUEST;
+
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
+            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, SCOPE_REQUEST);
+        }
+
+        ApiResponse apiResponse = ApiResponse.builder().error(
+                ExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(status.value())
+                        .title(ex.getCause().getMessage())
+                        .description(ex.getMessage())
+                        .build()
+        ).build();
+
+        return new ResponseEntity<>(apiResponse, headers, status);
+    }
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ApiResponse> handlerBusinessException(BusinessException be) {
+    public ResponseEntity<ApiResponse> handleBusinessException(BusinessException be) {
         return new ResponseEntity<>(
                 ApiResponse.builder().error(
                         BusinessExceptionDetails.builder()
@@ -33,8 +61,9 @@ public class RestExceptionHandler {
         );
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
         List<FieldError> errors = exception.getBindingResult().getFieldErrors();
         String fields = errors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
         String fieldsMessages = errors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
